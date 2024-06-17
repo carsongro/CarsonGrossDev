ddf2079428289bd34a7d1a6496537cf1ec117e7e
Carson Gross <carsongro@gmail.com>
2024-06-16 18:07:18 -0600

    Updated about me

--- Sources/Pages/Home.swift
+++ Sources/Pages/Home.swift
@@ -18,11 +18,7 @@ struct Home: StaticPage {
         Text {
             "Hi! I'm Carson and I build iOS apps and backends usually in Swift (including this website). I've built features for the Ancestry iOS and I'm an "
             Image(systemName: "apple")
-            " Swift Student Challenge 2024 Winner. I'm very passionate about building tools to "
-            Link("make full stack iOS development more efficient ", target: "https://github.com/carsongro/Ceto")
-            "and "
-            Link("animations in SwiftUI", target: "https://twitter.com/carsongrossdev/status/1783285909091463410")
-            ". I also have a B.S. in Business Administration with Computer Science Integration. If I'm not building stuff, I'm probably lifting weights, running, or reading!"
+            " Swift Student Challenge 2024 Winner. I'm very passionate about building things with Swift. If I'm not building stuff, I'm probably lifting weights, running, or reading!"
          }
         .font(.body)
     }
